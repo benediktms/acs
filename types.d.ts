e1e3c4287d04f1957f021df13267b9818518bdf6
Benedikt Schnatterbeck <benedikt.schnatterbeck@fresha.com>
2026-09-14 17:55:29 +0100

    fix(build): declare markdown text modules

--- types.d.ts
+++ types.d.ts
@@ -3,6 +3,11 @@ declare module "*.sql" {
   export default content;
 }
 
+declare module "*.md" {
+  const content: string;
+  export default content;
+}
+
 declare module "*CODEX_VERSION" {
   const content: string;
   export default content;
